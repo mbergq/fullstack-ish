4f68cea393e9e160c258db2ef7d9cafe4360b5a9
Martin <martinberg9@proton.me>
2024-10-25 07:46:01 +0200

    add axios and example get request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import "./index.css";
+import axios from "axios";
 
 type User = { userId: number; username: string }[];
 
 function App() {
-  const [data, setData] = useState<null | User>(null);
-
+  const [fetchData, setFetchData] = useState<null | User>(null);
   useEffect(() => {
-    fetch("/api/users")
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-
-        setData(result);
-      });
+    axios.get("api/users").then((res) => {
+      console.log(res.data);
+      setFetchData(res.data);
+    });
   }, []);
+  // useEffect(() => {
+  //   fetch("/api/users")
+  //     .then((response) => response.json())
+  //     .then((result) => {
+  //       console.log(result);
+
+  //       setData(result);
+  //     });
+  // }, []);
   return (
     <>
       <div className="min-h-dvh h-full w-full bg-slate-600">
-        <span>{data && data.map((res) => res.username)}</span>
+        <span>{fetchData && fetchData.map((res) => res.username)}</span>
       </div>
     </>
   );
